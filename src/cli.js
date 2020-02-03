c2c604f8597bfc1c40e59ba791b42c7dbb6df13d
Andre Greeff <agreeff@nortech.co.za>
2020-02-03 11:15:22 +0200

    wip: initial TS output option
    NOTE: this was a quick hack to include basic TS output file generation, while retaining the JS output file with the original CLI command.
    
    this way of parsing input arguments is hacky and brittle, so it will be reworked in the "Near Future".

--- src/cli.js
+++ src/cli.js
@@ -3,32 +3,64 @@
 const fs = require('fs');
 const path = require('path');
 
-const args = process.argv.slice(2);
-const inDir = args[0];
-const outFile = args[1];
-let output = 'this.pdfMake = this.pdfMake || {}; this.pdfMake.vfs = {';
+let args = process.argv.slice(2);
+let inDir, outFile, outFileExt, outFileWrapper;
 
 try {
+    switch (args.length) {
+        case 2:
+            outFileExt = '.js';
+            inDir = args[0];
+            outFile = args[1];
+            outFileWrapper = 'this.pdfMake = this.pdfMake || {};this.pdfMake.vfs = GENERATED_CONTENT';
+            break;
+        case 3:
+            outFileExt = (args[0] === '-t') ? '.ts' : '.js';
+            inDir = args[1];
+            outFile = args[2];
+            outFileWrapper = 'export default { pdfMake: { vfs: GENERATED_CONTENT } }';
+            break;
+        default:
+            throw new Error('incorrect arguments provided. expected "pdfmakefg [-t] <source-folder> <output-file[.extension]>"');
+    }
+
     if (!fs.existsSync(inDir)) {
         throw new Error(`${inDir} does not exists`);
     }
 
-    if (path.extname(outFile) !== '.js') {
-        throw new Error(`${outFile} must be a .js file`);
+    if (fs.statSync(inDir).isFile()) {
+        throw new Error(`${inDir} must be a folder`);
     }
-    
-    const readFiles = fs.readdirSync(inDir, { encoding: 'utf8' });
 
-    for (let filename of readFiles) {
-        const path = `${inDir}/${filename}`;
-        if (fs.lstatSync(path).isFile()) {
-            const fileContent = fs.readFileSync(path);
-            output += `"${filename}":"${fileContent.toString('base64')}",`;
+    if (path.extname(outFile).length === 0) {
+        outFile = `${outFile}${outFileExt}`;
+    }
+
+    if (path.extname(outFile) !== outFileExt) {
+        throw new Error(`${outFile} must be a ${outFileExt} file`);
+    }
+
+    // TODO: find Apple "ttc" and "dfont" formats to test with...
+    const inputExtPattern = /\.(ttf|otf|woff[2]?)$/; // |ttc|dfont
+    const readFiles = fs.readdirSync(inDir, {
+        encoding: 'utf8'
+    }).reduce((list, value) => {
+        if (value.match(inputExtPattern)) {
+            list.push(path.resolve(inDir, value));
         }
-    }
+        return list;
+    }, []);
 
-    fs.writeFileSync(outFile, output.substr(0, output.length -1) + '};');
+    const customFontsObject = readFiles.reduce((output, value) => {
+        const fileObject = path.parse(value);
+        output[fileObject.base] = fs.readFileSync(path.resolve(fileObject.dir, fileObject.base), {
+            encoding: 'base64'
+        });
+        return output;
+    }, {});
+
+    fs.writeFileSync(outFile, outFileWrapper.replace('GENERATED_CONTENT', JSON.stringify(customFontsObject)));
     console.log('The fonts were generated.');
 } catch (err) {
-    console.log(err.message);
+    console.log('error:', err.message || error);
 }
